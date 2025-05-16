e46c00a516b1e4c2d377ac2aa13e17e0556fc3e1
kuruneru <kuruneru.ikkou@gmail.com>
2025-05-16 20:09:17 +0900

    hii!

--- CPP/example.cpp
+++ CPP/example.cpp
@@ -1,20 +1,7 @@
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-
-  int A, B, i = 0; cin >> A >> B;
-  cout << "A:";
-  while (i < A) {
-    cout << "]";
-    ++i;
-  }
-  cout << "\nB:";
-  i = 0;
-  while (i < B) {
-    cout << "]";
-    ++i;
-  }
-  cout << endl;
-
+  
 }
