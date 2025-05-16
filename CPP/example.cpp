4b3842db3086a2175cada5f1b6e61cf71acda538
kuruneru <kuruneru.ikkou@gmail.com>
2025-05-16 18:54:18 +0900

    ファイル名を変更

---
+++ CPP/example.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int A, B, i = 0; cin >> A >> B;
+  cout << "A:";
+  while (i < A) {
+    cout << "]";
+    ++i;
+  }
+  cout << "\nB:";
+  i = 0;
+  while (i < B) {
+    cout << "]";
+    ++i;
+  }
+  cout << endl;
+
+}
--- CPP/practice.cpp
+++ CPP/practice.cpp
@@ -1,37 +1,34 @@
-#include <bits/stdc++.h>
+# include <iostream>
+# include <vector>
 using namespace std;
+using Graph = vector<vector<int>>;
+
+// 深さ優先探索
+vector<bool> seen;
+void dfs(const Graph &G, int v) {
+    seen[v] = true; // v を訪問済にする
+    
+    // v から行ける各頂点 next_v について
+    for (auto next_v : G[v]) { 
+        if (seen[next_v]) continue; // next_v が探索済だったらスルー
+        dfs(G, next_v); // 再帰的に探索
+    }
+}
 
 int main() {
-  // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
-  bool a = true; // true または false
-  bool b = false;// true または false
-  bool c = true;// true または false
+    // 頂点数と辺数
+    int N, M; cin >> N >> M;
 
-  // ここから先は変更しないこと
+    // グラフ入力受取 (ここでは無向グラフを想定)
+    Graph G(N);
+    for (int i = 0; i < M; ++i) {
+        int a, b;
+        cin >> a >> b;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
 
-  if (a) {
-    cout << "At";
-  }
-  else {
-    cout << "Yo";
-  }
-
-  if (!a && b) {
-    cout << "Bo";
-  }
-  else if (!b || c) {
-    cout << "Co";
-  }
-
-  if (a && b && c) {
-    cout << "foo!";
-  }
-  else if (true && false) {
-    cout << "yeah!";
-  }
-  else if (!a || c) {
-    cout << "der";
-  }
-
-  cout << endl;
+    // 頂点 0 をスタートとした探索
+    seen.assign(N, false); // 全頂点を「未訪問」に初期化
+    dfs(G, 0);
 }
