b84a4fa6dcaa3f6fcb010b4ac22705e413b5d8b1
kuruneru <kuruneru.ikkou@gmail.com>
2025-05-16 18:54:02 +0900

    "ファイル変更"

--- CPP/practice.cpp
+++ CPP/practice.cpp
@@ -1,12 +1,37 @@
-#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
 int main() {
-    string str; cin >> str;
-    for (int i = 0; i < str.size(); i++) {
-        str[i] -= 't' - 'g';
-        cout << str[i];
-    }
-    cout << endl;
+  // 変数a,b,cにtrueまたはfalseを代入してAtCoderと出力されるようにする。
+  bool a = true; // true または false
+  bool b = false;// true または false
+  bool c = true;// true または false
+
+  // ここから先は変更しないこと
+
+  if (a) {
+    cout << "At";
+  }
+  else {
+    cout << "Yo";
+  }
+
+  if (!a && b) {
+    cout << "Bo";
+  }
+  else if (!b || c) {
+    cout << "Co";
+  }
+
+  if (a && b && c) {
+    cout << "foo!";
+  }
+  else if (true && false) {
+    cout << "yeah!";
+  }
+  else if (!a || c) {
+    cout << "der";
+  }
+
+  cout << endl;
 }
